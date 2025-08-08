67fdb0bb470db2cdace4b85d5bc98d5949d138cb
Irene Ancilla Chow <ireneancilla@gmail.com>
2025-08-08 19:37:55 +0700

    initial xcode changes

---
+++ Moodly/Moodly/MoodlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodlyApp.swift
+//  Moodly
+//
+//  Created by Irene Ancilla Chow on 08/08/25.
+//
+
+import SwiftUI
+
+@main
+struct MoodlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
